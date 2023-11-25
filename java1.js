99c7fa0d714d739614e9566bb9a240ade7c149f2
salumian11 <salumian26@gmail.com>
2023-11-26 01:48:48 +0500

    Initial commit

---
+++ java1.js
@@ -0,0 +1,30 @@
+//COLOR FULL CAll..
+var x=document.getElementById("info")
+function green(){
+    x.style.color="Green";
+}
+function red(){
+    x.style.color="red";
+}
+function blue(){
+    x.style.color="blue";
+}
+function gray(){
+    x.style.color="gray";
+}
+function pink(){
+    x.style.color="pink";
+}
+function yellow(){
+    x.style.color="yellow";
+}
+function purple(){
+    x.style.color="purple";
+}
+function black(){
+    x.style.color="black";
+} 
+function brown(){
+    x.style.color="brown";
+}
+//COLOR FULL CAl..
